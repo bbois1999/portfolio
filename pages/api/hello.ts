8ef3cae9886e500313120c7fcbd8920379995514
bbois1999 <bbois1999@gmail.com>
2022-08-09 20:54:37 -0700

    testing

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,12 +1,7 @@
-import dbConnect from '../../lib/dbConnect'
-import Message from '../../models/Message'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { method } = req
-
-  await dbConnect()
   try {
     res.status(200).json({ success: true, data: 'HELLO' })
   } catch (error) {
