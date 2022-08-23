7c16dcc19df917fbb77dd80c1af2aef62415a40a
bbois1999 <bbois1999@gmail.com>
2022-08-23 14:01:59 -0700

    deleted that friggin pesky api page

--- pages/codesnip/[id].tsx
+++ pages/codesnip/[id].tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import { fallback } from "google-gax/build/src/fallback";
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 
